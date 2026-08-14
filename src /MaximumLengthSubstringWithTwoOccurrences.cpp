819b8cef7ac1678c5228115ab37e280528706715
Riddhi Pachehara <Riddhipachehara@gmail.com>
2026-08-14 18:11:34 +0530

    Computed maximum substring length with at most two occurrences using sliding window
    Implement a function to find the maximum length of a substring with at most two distinct characters.

---
+++ src /MaximumLengthSubstringWithTwoOccurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        vector<int> freq(26, 0);
+        int left = 0;
+        int max_len = 0;
+
+        for (int right = 0; right < s.length(); ++right) {
+            freq[s[right] - 'a']++;
+
+            while (freq[s[rightA'a']--;
+                left++;
+            }
+
+            max_len = max(max_len, right - left + 1);
+        }
+
+        return max_len;
+    }
+};
